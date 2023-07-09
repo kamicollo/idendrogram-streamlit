d01ff5d90bbc3b116bab2562f3b046de9b464b9e
Aurimas Racas <mail@aurimas.eu>
2023-07-08 20:01:08 -0400

    streamlit component declarations

--- idendro-streamlit/__init__.py
+++ idendro-streamlit/__init__.py
@@ -1,106 +1,92 @@
+import json
 import os
+from typing import Optional
 import streamlit.components.v1 as components
+from idendro.containers import ClusterNode, Dendrogram
+from idendro.targets.json import to_json
 
-# Create a _RELEASE constant. We'll set this to False while we're developing
-# the component, and True when we're ready to package and distribute it.
-# (This is, of course, optional - there are innumerable ways to manage your
-# release process.)
-_RELEASE = False
-
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
-
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
-
-if not _RELEASE:
-    _component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("my_component"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
-        "my_component",
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
-        url="http://localhost:3001",
-    )
-else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to to the component's
-    # build directory:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("my_component", path=build_dir)
+from idendro.common import _check_nodes, _check_orientation, _check_scale
 
 
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
-def my_component(name, key=None):
-    """Create a new instance of "my_component".
+def to_streamlit(
+    dendrogram: Dendrogram,
+    orientation: str = "top",
+    show_nodes: bool = True,
+    height: float = 400,
+    width: float = 400,
+    scale: str = "linear",
+) -> Optional[ClusterNode]:
+    """Renders dendrogram object as a custom bi-directional Streamlit component
 
-    Parameters
-    ----------
-    name: str
-        The name of the thing we're saying hello to. The component will display
-        the text "Hello, {name}!"
-    key: str or None
-        An optional key that uniquely identifies this component. If this is
-        None, and the component's arguments are changed, the component will
-        be re-mounted in the Streamlit frontend and lose its current state.
+    Args:
+        dendrogram (Dendrogram): IDendro dendrogram object
+        orientation (str, optional): Position of dendrogram's root node. One of "top", "bottom", "left", "right". Defaults to "top".
+        show_nodes (bool, optional): Whether to draw nodes. Defaults to True.
+        height (float, optional): Height of the dendrogram. Defaults to 400.
+        width (float, optional): Width of the dendrogram. Defaults to 400.
+        scale (str, optional): Scale used for the value axis. One of "linear", "symlog", "log". Defaults to 'linear'.
 
-    Returns
-    -------
-    int
-        The number of times the component's "Click Me" button has been clicked.
-        (This is the value passed to `Streamlit.setComponentValue` on the
-        frontend.)
-
+    Returns:
+        Optional[ClusterNode]: A ClusterNode object that was clicked on (None if no clicks took place)
     """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = _component_func(name=name, key=key, default=0)
 
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
-    return component_value
+    _check_orientation(dendrogram, orientation)
+    _check_scale(dendrogram, scale)
+    _check_nodes(dendrogram, show_nodes)
+
+    return StreamlitConverter(release=False).convert(
+        dendrogram=dendrogram,
+        orientation=orientation,
+        show_nodes=show_nodes,
+        height=height,
+        width=width,
+        key="idendro",
+        scale=scale,
+    )
 
 
-# Add some test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run my_component/__init__.py`
-if not _RELEASE:
-    import streamlit as st
+class StreamlitConverter:
+    def __init__(self, release: bool = True) -> None:
+        """Upon initialization, setup appropriate Streamlit component"""
 
-    st.subheader("Component with constant args")
+        if not release:
+            _component_func = components.declare_component(
+                "idendro",
+                url="http://localhost:3001",
+            )
+        else:
+            parent_dir = os.path.dirname(os.path.abspath(__file__))
+            build_dir = os.path.join(parent_dir, "frontend/build")
+            _component_func = components.declare_component("idendro", path=build_dir)
 
-    # Create an instance of our component with a constant `name` arg, and
-    # print its output value.
-    num_clicks = my_component("World")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
+        self.component_func = _component_func
 
-    st.markdown("---")
-    st.subheader("Component with variable args")
+    def convert(
+        self,
+        dendrogram: Dendrogram,
+        orientation: str,
+        show_nodes: bool,
+        width: float,
+        height: float,
+        scale: str,
+        key: Optional[str],
+    ) -> Optional[ClusterNode]:
+        """Renders the Streamlit component"""
 
-    # Create a second instance of our component whose `name` arg will vary
-    # based on a text_input widget.
-    #
-    # We use the special "key" argument to assign a fixed identity to this
-    # component instance. By default, when a component's arguments change,
-    # it is considered a new instance and will be re-mounted on the frontend
-    # and lose its current state. In this case, we want to vary the component's
-    # "name" argument without having it get recreated.
-    name_input = st.text_input("Enter a name", value="Streamlit")
-    num_clicks = my_component(name_input, key="foo")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
+        #ugly way to deal with streamlit not knowing how to serialize dataclasses
+        dendrogram = json.loads(to_json(dendrogram)) 
+
+        returned = self.component_func(
+            dendrogram=dendrogram,
+            orientation=orientation,
+            show_nodes=show_nodes,
+            width=width,
+            height=height,
+            key=key,
+            default=None,
+            scale=scale,
+        )
+        if returned is not None:
+            return ClusterNode(**returned)
+        else:
+            return None
