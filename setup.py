197a95873ac4c39cdb2331b0b2614439163f7f5d
Aurimas Racas <mail@aurimas.eu>
2023-07-08 20:06:28 -0400

    Setup prep

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
 setuptools.setup(
-    name="streamlit-custom-component",
-    version="0.0.1",
-    author="",
-    author_email="",
-    description="",
-    long_description="",
+    name="idendro-streamlit",
+    version="0.1",
+    author="Aurimas Racas",
+    author_email="mail@aurimas.eu",
+    description="Bi-directional Streamlit component for creating interactive dendrograms",
+    long_description="D3-powered bi-directional Streamlit component for idendro library that visualizes dendrograms created by hierarchical clustering algorithms (SciPy, scikit-learn and hdbscan compatible)",
     long_description_content_type="text/plain",
-    url="",
+    url="https://github.com/kamicollo/idendro-streamlit",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         # By definition, a Custom Component depends on Streamlit.
         # If your component has other Python dependencies, list
         # them here.
-        "streamlit >= 0.63",
+        "streamlit >= 0.63"        
     ],
 )
