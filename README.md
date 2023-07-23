e0ddbaa32f5e5fdce736a1d52e1daf57b4d19686
Aurimas Racas <mail@aurimas.eu>
2023-07-23 17:25:43 -0400

    Reorg

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-TBD
+# idendrogram-streamlit-component
+
+A D3-powered bi-directional Streamlit component for idendrogram library that visualizes dendrograms created by hierarchical clustering algorithms (SciPy, scikit-learn and hdbscan compatible).
+
+Demo [TBD]
+
+Docs @ [xxx]
+
+## Customizing the component
--- idendrogram_streamlit_component/__init__.py
+++ idendrogram_streamlit_component/__init__.py
@@ -1,9 +1,14 @@
+"""Bi-directional Streamlit component for creating interactive dendrograms"""
+
+__version__ = "0.1.4"
+
+
 import json
 import os
 from typing import Optional
 import streamlit.components.v1 as components
-from idendro.containers import ClusterNode, Dendrogram
-from idendro.targets.json import to_json
+from idendrogram.containers import ClusterNode, Dendrogram
+from idendrogram.targets.json import to_json
 
 class StreamlitConverter:
     def __init__(self, release: bool = True) -> None:
@@ -17,7 +22,7 @@ class StreamlitConverter:
         else:
             parent_dir = os.path.dirname(os.path.abspath(__file__))
             build_dir = os.path.join(parent_dir, "frontend/build")
-            _component_func = components.declare_component("idendro", path=build_dir)
+            _component_func = components.declare_component("idendrogram", path=build_dir)
 
         self.component_func = _component_func
 
